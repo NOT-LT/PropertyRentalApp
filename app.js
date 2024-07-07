b22deb36eef64f439cd4a4eb7e9961c3b81329c9
taljamri <t.aljamri55@gmail.com>
2024-07-08 00:48:50 +0300

    Adding basic CRUD

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const Property = require('./models/property');
 const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/propertyRentalApp',
@@ -10,20 +11,61 @@ mongoose.connect('mongodb://localhost:27017/propertyRentalApp',
 );
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", ()=> {
+db.once("open", () => {
   console.log("Database connected");
 })
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 
-app.get('/', async (req,res)=> {
-  const prop1 = new Property({title: 'First Property', price: 99999});
+app.get('/', async (req, res) => {
+  const prop1 = new Property({ title: 'First Property', price: 99999 });
   await prop1.save();
   res.send(prop1);
 })
 
-app.listen(3000, ()=> {
+app.get('/properties', async (req, res) => {
+  const properties = await Property.find({});
+  res.render('properties/index', { properties });
+})
+
+app.get ('/properties/new', (req,res)=> {
+  res.render('properties/new');
+})
+
+app.get('/properties/:id', async (req, res) => {
+  const { id } = req.params;
+  const property = await Property.findById(id);
+  res.render('properties/show', { property })
+})
+
+app.get('/properties/:id/edit', async(req,res)=> {
+  const { id } = req.params;
+  const property = await Property.findById(id);
+  res.render('properties/edit', {property})
+})
+
+app.post('/properties', async (req,res)=> {
+const property = new Property(req.body.property);
+await property.save();
+res.redirect(`/properties/${property._id}`)
+})
+
+app.put('/properties/:id', async (req,res)=>{
+  const {id} = req.params;
+  const property = await Property.findByIdAndUpdate(id, {...req.body.property});
+  res.redirect(`/properties/${id}`)
+})
+
+app.delete('/properties/:id', async (req,res)=> {
+  const {id} = req.params;
+  await Property.findByIdAndDelete(id);
+  res.redirect('/properties')
+})
+
+app.listen(3000, () => {
   console.log("Listening at port 3000");
 })
